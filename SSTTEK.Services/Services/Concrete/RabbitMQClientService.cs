7b482f004f95b5a92cfefac11a752ec12dc4eea9
Mustafa Kocatepe <msaidkocatepe@gmail.com>
2022-06-22 13:29:00 +0300

    add RabbitMQ publisher service

---
+++ SSTTEK.Services/Services/Concrete/RabbitMQClientService.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSTTEK.Services.Services.Concrete
+{
+    public class RabbitMQClientService : IDisposable
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+        public static string ExchangeName = "MailDirectExchange";
+        public static string RoutingMail = "mail-route";
+        public static string QueueName = "queue-mail";
+
+        public RabbitMQClientService(ConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+
+            if (_channel is { IsOpen: true })
+            {
+                return _channel;
+            }
+
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare(ExchangeName, type: "direct", true, false);
+
+            _channel.QueueDeclare(queue: QueueName,
+                                  durable: true,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+            _channel.QueueBind(exchange: ExchangeName,
+                               queue: QueueName,
+                               routingKey: RoutingMail);
+
+            return _channel;
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel?.Dispose();
+            _connection?.Close();
+            _connection?.Dispose();
+        }
+    }
+}
---
+++ SSTTEK.Services/Services/Concrete/RabbitMQPublisherService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace SSTTEK.Services.Services.Concrete
+{
+    public class RabbitMQPublisherService
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+
+        public RabbitMQPublisherService(RabbitMQClientService rabbitMQClientService)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+        }
+
+        public void Publish( model) //TODO : M
+        {
+            var channel = _rabbitMQClientService.Connect();
+
+            var bodyString = JsonSerializer.Serialize(model);
+
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
+
+            var properties = channel.CreateBasicProperties();
+
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: RabbitMQClientService.ExchangeName,
+                                routingKey: RabbitMQClientService.RoutingMail,
+                                basicProperties: properties,
+                                body: bodyByte);
+        }
+    }
+}
